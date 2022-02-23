ac122965622679b1d6020ef51a2912fb633d6bac
Abdul <abdulwahabdev0@gmail.com>
2022-02-23 14:28:20 +0500

    stlye-src: 'self' *

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(
     useDefaults: false,
     directives: {
       "default-src": ["'self'", "'unsafe-inline'"],
-      "style-src": ["'self'", "'unsafe-inline'"],
+      "style-src": ["'self' *", "'unsafe-inline'"],
       "img-src": ["'self' data: blob:", "futjan.s3.ap-south-1.amazonaws.com"],
+      "font-src": ["'self' https: data:", "fonts.googleapis.com"],
     },
   })
 );
